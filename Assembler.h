5b193b11cc165508fc4364b5c421783f2ba2d5b7
Tianhe Zhang <62363213+WAHearer@users.noreply.github.com>
2024-11-21 23:49:25 +0800

    Update Assembler.h

--- Assembler.h
+++ Assembler.h
@@ -12,7 +12,7 @@ private:
     static std::string regNameToStrBin(const char &c);//translate register names to 3-bit binary
     static long long numToDex(const std::string &num);//translate decimal or hexadecimal number to long long decimal number
     static std::string numToStrBin(const std::string &num,int bits);//translate decimal or hexadecimal number to 2's complement
-    void capitalize(std::string &code);//capitalize all letters
+    void capitalize(std::string &code);//capitalize all letters and replace tabs with spaces
     void deleteSpaceAndComment(std::string &code);//delete unnecessary spaces and comments
     static void cut(std::vector<std::string>&code);//cut the program from .ORIG to .END and delete blank line
     static void syntaxCheck(const std::vector<std::string>&code);
