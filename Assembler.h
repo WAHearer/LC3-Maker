e0aa7d7c2b418f4017f84763f671b60b6fb48a33
Tianhe Zhang <62363213+WAHearer@users.noreply.github.com>
2024-11-22 21:48:47 +0800

    Update Assembler.h

--- Assembler.h
+++ Assembler.h
@@ -10,14 +10,14 @@ class Assembler{
 private:
     std::map<std::string,std::string>oprandToBinary;
     std::map<std::string,bool>isPseudoInstruction;
-    static std::string regNameToStrBin(const char &c);//translate register names to 3-bit binary
-    static long long numToDex(const std::string &num);//translate decimal or hexadecimal number to long long decimal number
-    static std::string numToStrBin(const std::string &num,int bits);//translate decimal or hexadecimal number to 2's complement
-    void capitalize(std::vector<std::string>&code);//capitalize all letters and replace tabs with spaces
-    void deleteSpaceAndComment(std::vector<std::string>&code);//delete unnecessary spaces and comments
+    static std::string regNameToStrBin(const char &c);//transform register names to 3-bit binary
+    static int numToDex(const std::string &num);//transform string type decimal or hexadecimal number to int type decimal number
+    static std::string numToStrBin(const std::string &num,int bits);//transform decimal or hexadecimal number to 2's complement
+    static void capitalize(std::vector<std::string>&code);//capitalize all letters and replace tab with space
+    void deleteSpaceAndComment(std::vector<std::string>&code);//delete unnecessary space and comment
     static void cut(std::vector<std::string>&code);//cut the program from .ORIG to .END and delete blank line
     static void syntaxCheck(const std::vector<std::string>&code);//check the syntax of the program
-    std::map<std::string,long long>linkLabel(const std::vector<std::string>&code);//translate labels to addresses
+    std::map<std::string,int>linkLabel(const std::vector<std::string>&code);//link label with address
 public:
     Assembler();
     std::vector<std::string>assemble(std::vector<std::string>code);
