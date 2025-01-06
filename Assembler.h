0de09dc85f85a0619cdaed527260b1299466debc
Tianhe Zhang <62363213+WAHearer@users.noreply.github.com>
2025-01-06 19:04:27 +0800

    Add files via upload

--- Assembler.h
+++ Assembler.h
@@ -1,22 +1,24 @@
-#include<string>
+#include<cstring>
 #include<vector>
 #include<map>
 #include<sstream>
 #include<regex>
 #include<iostream>
 #include<conio.h>
+#include<thread>
+#include<functional>
 #ifndef LC3_ASSEMBLER_ASSEMBLER_H
 #define LC3_ASSEMBLER_ASSEMBLER_H
 class Assembler{
 private:
     std::map<std::string,std::string>oprandToBinary;
     std::map<std::string,bool>isPseudoInstruction;
-    static std::string regNameToStrBin(const char &c);//generate 3-bit register name
-    static int numToDex(const std::string &num);//transform string type decimal or hexadecimal number to int type decimal number
-    static std::string numToStrBin(const std::string &num,int bits);//transform decimal or hexadecimal number to 2's complement
-    static void capitalize(std::vector<std::string>&code);//capitalize all letters and replace tab with space
+    std::string regNameToStrBin(const char &c);//generate 3-bit register name
+    int numToDex(const std::string &num);//transform string type decimal or hexadecimal number to int type decimal number
+    std::string numToStrBin(const std::string &num,int bits);//transform decimal or hexadecimal number to 2's complement
+    void capitalize(std::vector<std::string>&code);//capitalize all letters and replace tab with space
     void deleteSpaceAndComment(std::vector<std::string>&code);//delete unnecessary space and comment
-    static void cut(std::vector<std::string>&code);//cut the program from .ORIG to .END
+    void cut(std::vector<std::string>&code);//cut the program from .ORIG to .END
     void syntaxCheck(const std::vector<std::string>&code);//check the syntax of the program
     std::map<std::string,int>linkLabel(const std::vector<std::string>&code);//link label with address
 public:
@@ -25,9 +27,14 @@ public:
 };
 class Runner{
 private:
-    static short binToNum(const std::string &num);//transform 16-bit binary to decimal
-    static std::string numToBin(short num);//transform decimal to 16-bit binary
+    short memory[65536],reg[8];
+    bool PSR[16];
+    unsigned short pc,SAVED_SSP,SAVED_USP;
+    short binToNum(const std::string &num);//transform 16-bit binary to decimal
+    std::string numToBin(short num);//transform decimal to 16-bit binary
+    void load(std::vector<std::string>&code);
 public:
+    Runner();
     void run(std::vector<std::string>code);
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
+#include<fstream>
 #include<vector>
 #include"Assembler.h"
 using std::cin,std::cout,std::endl;
 int main(){
+    setbuf(stdout, 0);
     Assembler assembler;
-    freopen("code.txt","r",stdin);
+    std::ifstream ifs("code.txt");
     std::vector<std::string>code;
     std::string strInput;
-    while(std::getline(cin,strInput)){
+    while(std::getline(ifs,strInput)){
         code.push_back(strInput);
     }
     std::vector<std::string>binCode=assembler.assemble(code);
